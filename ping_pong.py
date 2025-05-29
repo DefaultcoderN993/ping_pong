b5fa5eb6b051d9b393a2f878f2fd6467dde8ddfc
DefaultcoderN993 <dmitriy.sedov2013@gmail.com>
2025-05-29 15:06:34 +0300

    v1.2 (dev) : игроки двигаются

--- ping_pong.py
+++ ping_pong.py
@@ -1,12 +1,13 @@
-from pygame import *
+import pygame as pg
 
-window = display.set_mode((700, 500))
-clock = time.Clock()
-display.set_caption('Ping Pong')
-class GameSprite(sprite.Sprite):
+window = pg.display.set_mode((700, 500))
+clock = pg.time.Clock()
+pg.display.set_caption('Ping Pong')
+
+class GameSprite(pg.sprite.Sprite):
     def __init__(self, speed, p_image, x, y, scale_x, scale_y):
         super().__init__()
-        self.image = transform.scale(image.load(p_image), (scale_x, scale_y))
+        self.image = pg.transform.scale(pg.image.load(p_image), (scale_x, scale_y))
         self.speed = speed
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -16,15 +17,31 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def update(self):
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_d] and self.rect.x < 595:
-            self.rect.x += self.speed
+        keys_pressed = pg.key.get_pressed()
+        if keys_pressed[pg.K_w] and self.rect.y > 10:
+            self.rect.y -= self.speed
+        if keys_pressed[pg.K_s] and self.rect.y < 395:
+            self.rect.y += self.speed
+
+class Player2(GameSprite):
+    def update(self):
+        keys_pressed = pg.key.get_pressed()
+        if keys_pressed[pg.K_UP] and self.rect.y > 10:
+            self.rect.y -= self.speed
+        if keys_pressed[pg.K_DOWN] and self.rect.y < 395:
+            self.rect.y += self.speed
+
+player1 = Player(7, 'рокетка.png', 650, 250, 50, 100)
+player2 = Player2(7, 'рокетка.png', 50, 250, 50, 100)
+
 game = True
 while game:
-	for e in event.get():
-        	if e.type == QUIT:
-            		game = False
-	clock.tick(40)
-	display.update()
+    player1.draw()
+    player2.draw()
+    for e in pg.event.get():
+        if e.type == pg.QUIT:
+            game = False
+    player1.update()
+    player2.update()
+    clock.tick(40)
+    pg.display.update()
