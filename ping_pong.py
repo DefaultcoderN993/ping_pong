1b09adc669860eb5615b6fbe98c9c324168f63da
DefaultcoderN993 <dmitriy.sedov2013@gmail.com>
2025-05-29 15:33:33 +0300

    v1.2.3 (dev) : немного багов

--- ping_pong.py
+++ ping_pong.py
@@ -4,16 +4,24 @@ window = pg.display.set_mode((700, 500))
 clock = pg.time.Clock()
 pg.display.set_caption('Ping Pong')
 
+BLACK = (0, 0, 0)
+
 class GameSprite(pg.sprite.Sprite):
-    def __init__(self, speed, p_image, x, y, scale_x, scale_y):
+    def __init__(self, speed, p_image, x, y, scale_x, scale_y, bg_color=BLACK):
         super().__init__()
         self.image = pg.transform.scale(pg.image.load(p_image), (scale_x, scale_y))
         self.speed = speed
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.bg_color = bg_color
     def draw(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+    def fill(self, color=None):
+        if color is None:
+            pg.draw.rect(window, self.bg_color, self.rect)
+        else:
+            pg.draw.rect(window, color, self.rect)
 
 class Player(GameSprite):
     def update(self):
@@ -41,6 +49,8 @@ while game:
     for e in pg.event.get():
         if e.type == pg.QUIT:
             game = False
+    player1.fill()
+    player2.fill()
     player1.update()
     player2.update()
     clock.tick(40)
